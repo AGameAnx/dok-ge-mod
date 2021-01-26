cc98b67ab2e18b3f06a3f71dbd587494114429e8
AGameAnx <agameanx@gmail.com>
2021-01-27 01:34:26 +0200

    Added BBI.Unity.Game assemby file as a modified file

---
+++ BBI.Unity.Game.cs
@@ -0,0 +1,36 @@
+// C:\Games\Steam\steamapps\common\Deserts of Kharak\Data\Managed\BBI.Unity.Game.dll
+// BBI.Unity.Game, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+
+// Timestamp: 5C0AD8D7 (2018-12-07 22:32:23)
+
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using UnityEngine;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: AssemblyCompany("Blackbird Interactive")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCopyright("Copyright © Blackbird Interactive 2014")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("v1.3.0 [b][ GE mod 1.2 ]")]
+[assembly: AssemblyProduct("BBI.Unity.Game")]
+[assembly: AssemblyTitle("BBI.Unity.Game")]
+[assembly: AssemblyTrademark("")]
+[assembly: CompilationRelaxations(8)]
+[assembly: InternalsVisibleTo("BBI.Unity.Tests")]
+[assembly: InternalsVisibleTo("BBI.Debugger")]
+[assembly: InternalsVisibleTo("BBI.Game.uScriptNodes.Tests")]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: ComVisible(false)]
+[assembly: Guid("5b5b445b-fc18-4d62-af2b-2ff78efd4b22")]
+[assembly: UnityAPICompatibilityVersion("5.2.2f1")]
+[assembly: UnityAPICompatibilityVersion("5.2.0f3")]
+[assembly: UnityAPICompatibilityVersion("5.0.2f1")]
+[assembly: UnityAPICompatibilityVersion("5.2.3f1")]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
