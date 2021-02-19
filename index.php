8ed3126ed04311e1ea294a94fcadfce619c582a4
AGameAnx <agameanx@gmail.com>
2021-02-19 18:27:42 +0200

    File used for proxy requests to github when applying patches and layouts

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$patch = basename($_GET['p']);
+$layout = basename($_GET['l']);
+
+$url = "";
+
+if ($patch) {
+	$url = "https://raw.githubusercontent.com/AGameAnx/dok-repo/master/patches/" . $patch . ".json";
+} elseif ($layout) {
+	$url = "https://raw.githubusercontent.com/AGameAnx/dok-repo/master/layouts/" . $layout . ".dokmap";
+}
+
+if (strlen($url) <= 0) {
+	http_response_code(403);
+	die('Forbidden');
+}
+
+echo file_get_contents($url);
