bbf92e8fc229fe5e5e4974d6dd7c90f8de157aff
AGameAnx <agameanx@gmail.com>
2023-10-22 14:39:07 +0300

    GG.EpicGames.EpicGamesPlayerIDManager.Fixed 'INVALID' display names in replays

--- GG.EpicGames/EpicGamesPlayerIDManager.GetDisplayName.cs
+++ GG.EpicGames/EpicGamesPlayerIDManager.GetDisplayName.cs
@@ -8,18 +8,13 @@ namespace GG.EpicGames
 	{
 		public override string GetDisplayName(NetworkPlayerID playerID)
 		{
-			string result = "'INVALID'";
 			if (playerID == NetworkPlayerID.kInvalidID)
 			{
 				Log.Warn(Log.Channel.Online, "GetGamePlatformType: playerID was invalid.", new object[0]);
-				return result;
+				return "'INVALID'";
 			}
 			CachedUserInfo cachedUserInfo = EpicUserIntegration.GetCachedUserInfo(playerID.ID, false);
-			if (cachedUserInfo != null)
-			{
-				result = cachedUserInfo.DisplayName;
-			}
-			return result;
+			return cachedUserInfo == null ? null : cachedUserInfo.DisplayName;
 		}
 	}
 }
